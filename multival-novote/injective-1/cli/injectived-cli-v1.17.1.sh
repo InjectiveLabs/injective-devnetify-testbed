65d8ca54323d6205463fd1cfe9a5b90ad56bf491
Max <maxim@injectivelabs.org>
2026-01-18 23:26:39 +0400

    chore: add v1.17.1

---
+++ multival-novote/injective-1/cli/injectived-cli-v1.17.1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -it --rm \
+    -v $(pwd)/injective-1:/apps/data/injective-1 \
+    injectivelabs/injective-core:v1.17.1 injectived \
+    --home "./injective-1/validators/0" \
+    ${*:-"query"}
